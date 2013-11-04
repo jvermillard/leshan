7e9e17681759b7753e7b7f1c4d1e851b7034e9c5
Manuel Sangoi <msangoi@sierrawireless.com>
2013-11-04 14:22:34 +0100

    Simple UI to list connected clients and read some resources

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* App Module */
+
+var lwClientApp = angular.module('lwClientApp', [ 'ngRoute',
+		'lwClientControllers' ]);
+
+lwClientApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	
+	$routeProvider.
+		when('/clients', { templateUrl : 'partials/client-list.html', controller : 'ClientListCtrl'	}).
+		when('/clients/:clientId', { templateUrl : 'partials/client-detail.html', controller : 'ClientDetailCtrl' }).
+		otherwise({ redirectTo : '/clients'	});
+	
+} ]);
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,20 +1,53 @@
-var lwClientApp = angular.module('lwClientApp', []);
+var lwClientControllers = angular.module('lwClientControllers', []);
 
-lwClientApp.controller('LwClientCtrl', function LwClientCtrl($scope, $http) {
+lwClientControllers.controller('ClientListCtrl', [
+		'$scope',
+		'$http',
+		function ClientListCtrl($scope, $http) {
 
-	// get the list of connected clients
-	$http.get('/api/clients').success(function(data, status, headers, config) {
-		$scope.clients = data;
-	});
+			// get the list of connected clients
+			$http.get('/api/clients').success(
+					function(data, status, headers, config) {
+						$scope.clients = data;
+					});
 
-	// listen for new connections
-	var handleCallback = function(msg) {
-		$scope.$apply(function() {
-			var client = JSON.parse(msg.data);
-			$scope.clients.push(client);
-		});
-	}
+			// listen for new connections
+			var handleCallback = function(msg) {
+				$scope.$apply(function() {
+					var client = JSON.parse(msg.data);
+					$scope.clients.push(client);
+				});
+			}
 
-	var source = new EventSource('/event');
-	source.addEventListener('REGISTRATION', handleCallback, false);
-});
+			var source = new EventSource('/event');
+			source.addEventListener('REGISTRATION', handleCallback, false);
+		} ]);
+
+lwClientControllers.controller('ClientDetailCtrl', [ '$scope', '$location', '$anchorScroll', '$routeParams', '$http',
+		function($scope, $location, $anchorScroll, $routeParams, $http) {
+			$scope.clientId = $routeParams.clientId;
+			
+			$scope.scrollTo = function(id) {
+			     $location.hash(id);
+			     $anchorScroll();
+			  };
+			  
+			// get the lw resources description (static for now)
+			$http.get('/json/lw-resources.json').success(
+				function(data, status, headers, config) {
+					$scope.lwresources = data;
+				});
+			
+			$scope.read = function(resource, idx) {
+				
+				// TODO read for full object
+				$http.get("/api/clients/" + $scope.clientId + "/" + resource)
+					.success(function(data, status, headers, config) {
+						//alert(JSON.stringify(data));
+						if(data.status = "CONTENT") {
+							$scope.lwresources[idx].value = data.value;
+						}
+					});
+				};
+			
+		} ]);
