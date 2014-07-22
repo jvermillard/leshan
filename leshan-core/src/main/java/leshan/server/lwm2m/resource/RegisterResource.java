fb4c6fd15de42b0c9bf7f437c8729e3e717bf55f
Julien Vermillard <jvermillard@sierrawireless.com>
2014-07-22 09:25:20 +0200

    killing DTLS session in case of bad authentication

--- leshan-core/src/main/java/leshan/server/lwm2m/resource/RegisterResource.java
+++ leshan-core/src/main/java/leshan/server/lwm2m/resource/RegisterResource.java
@@ -148,9 +148,15 @@ public class RegisterResource extends ResourceBase {
                             registrationEndpoint, pskIdentity);
 
                     if (securityInfo == null || pskIdentity == null || !pskIdentity.equals(securityInfo.getIdentity())) {
-                        LOG.warn("Invalid identity for client {}: expected '{}'but was '{}'", endpoint,
-                                securityInfo.getIdentity(), pskIdentity);
+                        LOG.warn("Invalid identity for client {}: expected '{}' but was '{}'", endpoint,
+                                securityInfo == null ? null : securityInfo.getIdentity(), pskIdentity);
                         exchange.respond(ResponseCode.BAD_REQUEST, "Invalid identity");
+
+                        // kill the TLS Session
+                        ((SecureEndpoint) exchange.advanced().getEndpoint()).getDTLSConnector().close(
+                                new InetSocketAddress(request.getSource(), request.getSourcePort()));
+                        return;
+
                     } else {
                         LOG.debug("authenticated client {} using DTLS PSK", endpoint);
                     }
