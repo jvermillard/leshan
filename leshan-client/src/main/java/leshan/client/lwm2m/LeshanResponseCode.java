c962a818ce472342942712514df6a29981520873
Andrew Summers <asummers@zebra.com>
2014-08-21 11:01:40 -0500

    Reduced duplication for proxy

--- leshan-client/src/main/java/leshan/client/lwm2m/LeshanResponseCode.java
+++ leshan-client/src/main/java/leshan/client/lwm2m/LeshanResponseCode.java
@@ -4,44 +4,44 @@ import ch.ethz.inf.vs.californium.coap.CoAP.ResponseCode;
 
 public enum LeshanResponseCode {	
 	// Success
-	CREATED(65),
-	DELETED(66),
-	VALID(67),
-	CHANGED(68),
-	CONTENT(69),
-	CONTINUE(95),
+	CREATED(ResponseCode.CREATED),
+	DELETED(ResponseCode.DELETED),
+	VALID(ResponseCode.VALID),
+	CHANGED(ResponseCode.CHANGED),
+	CONTENT(ResponseCode.CONTENT),
+	CONTINUE(ResponseCode.CONTINUE),
 
 	// Client error
-	BAD_REQUEST(128),
-	UNAUTHORIZED(129),
-	BAD_OPTION(130),
-	FORBIDDEN(131),
-	NOT_FOUND(132),
-	METHOD_NOT_ALLOWED(133),
-	NOT_ACCEPTABLE(134),
-	REQUEST_ENTITY_INCOMPLETE(136),
-	PRECONDITION_FAILED(140),
-	REQUEST_ENTITY_TOO_LARGE(141), 
-	UNSUPPORTED_CONTENT_FORMAT(143),
+	BAD_REQUEST(ResponseCode.BAD_REQUEST),
+	UNAUTHORIZED(ResponseCode.UNAUTHORIZED),
+	BAD_OPTION(ResponseCode.BAD_OPTION),
+	FORBIDDEN(ResponseCode.FORBIDDEN),
+	NOT_FOUND(ResponseCode.NOT_FOUND),
+	METHOD_NOT_ALLOWED(ResponseCode.METHOD_NOT_ALLOWED),
+	NOT_ACCEPTABLE(ResponseCode.NOT_ACCEPTABLE),
+	REQUEST_ENTITY_INCOMPLETE(ResponseCode.REQUEST_ENTITY_INCOMPLETE),
+	PRECONDITION_FAILED(ResponseCode.PRECONDITION_FAILED),
+	REQUEST_ENTITY_TOO_LARGE(ResponseCode.REQUEST_ENTITY_TOO_LARGE), 
+	UNSUPPORTED_CONTENT_FORMAT(ResponseCode.UNSUPPORTED_CONTENT_FORMAT),
 
 	// Server error
-	INTERNAL_SERVER_ERROR(160),
-	NOT_IMPLEMENTED(161),
-	BAD_GATEWAY(162),
-	SERVICE_UNAVAILABLE(163),
-	GATEWAY_TIMEOUT(164),
-	PROXY_NOT_SUPPORTED(165);
+	INTERNAL_SERVER_ERROR(ResponseCode.INTERNAL_SERVER_ERROR),
+	NOT_IMPLEMENTED(ResponseCode.NOT_IMPLEMENTED),
+	BAD_GATEWAY(ResponseCode.BAD_GATEWAY),
+	SERVICE_UNAVAILABLE(ResponseCode.SERVICE_UNAVAILABLE),
+	GATEWAY_TIMEOUT(ResponseCode.GATEWAY_TIMEOUT),
+	PROXY_NOT_SUPPORTED(ResponseCode.PROXY_NOT_SUPPORTED);
 	
 	/** The code value. */
-	public final int value;
+	private final ResponseCode responseCode;
 	
 	/**
 	 * Instantiates a new response code with the specified integer value.
 	 *
 	 * @param value the integer value
 	 */
-	private LeshanResponseCode(int value) {
-		this.value = value;
+	private LeshanResponseCode(ResponseCode responseCode) {
+		this.responseCode = responseCode;
 	}
 	
 	/**
@@ -52,54 +52,31 @@ public enum LeshanResponseCode {
 	 * @throws IllegalArgumentException if integer value is not recognized
 	 */
 	public static LeshanResponseCode valueOf(int value) {
-		switch (value) {
-			case 65: return CREATED;
-			case 66: return DELETED;
-			case 67: return VALID;
-			case 68: return CHANGED;
-			case 69: return CONTENT;
-			case 128: return BAD_REQUEST;
-			case 129: return UNAUTHORIZED;
-			case 130: return BAD_OPTION;
-			case 131: return FORBIDDEN;
-			case 132: return NOT_FOUND;
-			case 133: return METHOD_NOT_ALLOWED;
-			case 134: return NOT_ACCEPTABLE;
-			case 136: return REQUEST_ENTITY_INCOMPLETE;
-			case 140: return PRECONDITION_FAILED;
-			case 141: return REQUEST_ENTITY_TOO_LARGE;
-			case 143: return UNSUPPORTED_CONTENT_FORMAT;
-			case 160: return INTERNAL_SERVER_ERROR;
-			case 161: return NOT_IMPLEMENTED;
-			case 162: return BAD_GATEWAY;
-			case 163: return SERVICE_UNAVAILABLE;
-			case 164: return GATEWAY_TIMEOUT;
-			case 165: return PROXY_NOT_SUPPORTED;
-			default: // Make an extensive search
-				for (LeshanResponseCode code : LeshanResponseCode.values()) {
-					if (code.value == value) {
-						return code;
-					}
-				}
-				
-				throw new IllegalArgumentException("Unknown CoAP response code " + value);
+		ResponseCode responseCode = ResponseCode.valueOf(value);
+		
+		for (LeshanResponseCode code : LeshanResponseCode.values()) {
+			if (code.responseCode == responseCode) {
+				return code;
+			}
 		}
+		
+		throw new IllegalArgumentException("Unknown Leshan response code " + value);
 	}
 	
 	@Override
 	public String toString() {
-		return ResponseCode.valueOf(this.value).toString();
+		return this.responseCode.toString();
 	}
 	
 	public static boolean isSuccess(LeshanResponseCode code) {
-		return ResponseCode.isSuccess(ResponseCode.valueOf(code.value));
+		return ResponseCode.isSuccess(ResponseCode.valueOf(code.responseCode.value));
 	}
 	
 	public static boolean isClientError(LeshanResponseCode code) {
-		return ResponseCode.isClientError(ResponseCode.valueOf(code.value));
+		return ResponseCode.isClientError(ResponseCode.valueOf(code.responseCode.value));
 	}
 	
 	public static boolean isServerError(LeshanResponseCode code) {
-		return ResponseCode.isServerError(ResponseCode.valueOf(code.value));
+		return ResponseCode.isServerError(ResponseCode.valueOf(code.responseCode.value));
 	}
 }
