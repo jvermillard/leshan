042db96d1453fb6fd8b51ecac02cccb6908394e7
Josh Larson <jlarson@zebra.com>
2014-08-27 10:37:08 -0500

    Added client-side server and read test

--- leshan-standalone/src/test/java/leshan/server/stuff/Stuff.java
+++ leshan-standalone/src/test/java/leshan/server/stuff/Stuff.java
@@ -1,10 +1,17 @@
 package leshan.server.stuff;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.net.InetSocketAddress;
 
 import leshan.server.lwm2m.LwM2mServer;
 import leshan.server.lwm2m.bootstrap.BootstrapStoreImpl;
+import leshan.server.lwm2m.client.Client;
 import leshan.server.lwm2m.client.ClientRegistryImpl;
+import leshan.server.lwm2m.message.ClientResponse;
+import leshan.server.lwm2m.message.ReadRequest;
+import leshan.server.lwm2m.message.ResponseCode;
 import leshan.server.lwm2m.observation.ObservationRegistry;
 import leshan.server.lwm2m.observation.ObservationRegistryImpl;
 import leshan.server.lwm2m.security.SecurityRegistry;
@@ -12,24 +19,29 @@ import leshan.server.lwm2m.security.SecurityRegistry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import ch.ethz.inf.vs.californium.CoapClient;
 import ch.ethz.inf.vs.californium.CoapResponse;
+import ch.ethz.inf.vs.californium.network.Endpoint;
+import ch.ethz.inf.vs.californium.server.Server;
+import ch.ethz.inf.vs.californium.server.resources.CoapExchange;
+import ch.ethz.inf.vs.californium.server.resources.Resource;
+import ch.ethz.inf.vs.californium.server.resources.ResourceBase;
 
 public class Stuff {
 
+	private static final String LWM2M_SERVER_ADDRESS = "coap://localhost:5683/rd?ep=device1";
+
 	private final String VALID_REQUEST_PAYLOAD = "</lwm2m>;rt=\"oma.lwm2m\", </lwm2m/1/101>, </lwm2m/1/102>, </lwm2m/2/0>, </lwm2m/2/1>, </lwm2m/2/2>, </lwm2m/3/0>, </lwm2m/4/0>, </lwm2m/5>";
 
-	private static final Logger LOG = LoggerFactory.getLogger(Stuff.class);
 	private LwM2mServer server;
+	private ClientRegistryImpl clientRegistry;
 
 	@Before
 	public void setup() {
 		final InetSocketAddress address = new InetSocketAddress(5683);
 		final InetSocketAddress addressSecure = new InetSocketAddress(5684);
-		final ClientRegistryImpl clientRegistry = new ClientRegistryImpl();
+		clientRegistry = new ClientRegistryImpl();
 		final ObservationRegistry observationRegistry = new ObservationRegistryImpl();
 		final SecurityRegistry securityRegistry = new SecurityRegistry();
 		final BootstrapStoreImpl bsStore = new BootstrapStoreImpl();
@@ -43,10 +55,52 @@ public class Stuff {
 	}
 
 	@Test
-	public void registrationTest() {
-		final CoapClient client = new CoapClient("coap://localhost:5683/rd?ep=device1");
+	public void registrationRespondsWithCreated() {
+		final CoapClient client = new CoapClient(LWM2M_SERVER_ADDRESS);
 		final CoapResponse post = client.post(VALID_REQUEST_PAYLOAD, 1);
-		LOG.info("Result of POST: " + post);
+		assertEquals("", post.getResponseText());
+		assertEquals(ch.ethz.inf.vs.californium.coap.CoAP.ResponseCode.CREATED, post.getCode());
+	}
+
+	@Test
+	public void registeredDeviceCanHaveReadSentToIt() {
+		final CoapClient client = new CoapClient(LWM2M_SERVER_ADDRESS);
+
+		final Server clientSideServer = new Server(44022);
+		final Endpoint endpoint = clientSideServer.getEndpoint(44022);
+		client.setEndpoint(endpoint);
+
+		final Resource readResource = new ObjectResource(1);
+		clientSideServer.add(readResource);
+		clientSideServer.start();
+
+		client.post(VALID_REQUEST_PAYLOAD, 1);
+
+		final Client registeredClient = clientRegistry.get("device1");
+		assertNotNull(registeredClient);
+		final ClientResponse response = ReadRequest.newRequest(registeredClient, 1).send(server.getRequestHandler());
+
+		assertEquals(ResponseCode.CONTENT, response.getCode());
+		assertEquals("THIS SHOULD HAVE TLV STUFF", new String(response.getContent()));
+
+		clientSideServer.stop();
+	}
+
+}
+
+
+class ObjectResource extends ResourceBase {
+
+	private final String value;
+
+	public ObjectResource(final int objectId) {
+		super(Integer.toString(objectId));
+		value = "THIS SHOULD HAVE TLV STUFF";
+	}
+
+	@Override
+	public void handleGET(final CoapExchange exchange) {
+		exchange.respond(ch.ethz.inf.vs.californium.coap.CoAP.ResponseCode.CONTENT, value);
 	}
 
 }
