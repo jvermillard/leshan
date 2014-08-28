aee357f6f05c8c56366bf8c8f28fb81b0b3def1d
Josh Larson <jlarson@zebra.com>
2014-08-28 10:48:37 -0500

    Extracted resource classes to their own files

---
+++ leshan-client/src/main/java/leshan/client/lwm2m/resource/ClientObjectInstance.java
@@ -0,0 +1,43 @@
+package leshan.client.lwm2m.resource;
+
+import static ch.ethz.inf.vs.californium.coap.CoAP.ResponseCode.CONTENT;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import leshan.server.lwm2m.tlv.Tlv;
+import leshan.server.lwm2m.tlv.TlvEncoder;
+import ch.ethz.inf.vs.californium.server.resources.CoapExchange;
+import ch.ethz.inf.vs.californium.server.resources.Resource;
+import ch.ethz.inf.vs.californium.server.resources.ResourceBase;
+
+class ClientObjectInstance extends ResourceBase {
+
+	public ClientObjectInstance(final int instanceID, final Map<Integer, ClientResource> resources) {
+		super(Integer.toString(instanceID));
+		for(final Map.Entry<Integer, ClientResource> entry : resources.entrySet()){
+			add(entry.getValue());
+		}
+	}
+
+	public int getInstanceId() {
+		return Integer.parseInt(getName());
+	}
+
+	@Override
+	public void handleGET(final CoapExchange exchange) {
+		final Tlv[] tlvArray = asTlvArray();
+		exchange.respond(CONTENT, TlvEncoder.encode(tlvArray).array());
+	}
+
+	public Tlv[] asTlvArray() {
+		final List<Tlv> tlvs = new ArrayList<>();
+		for (final Resource res : getChildren()) {
+			tlvs.add(((ClientResource) res).asTlv());
+		}
+		final Tlv[] tlvArray = tlvs.toArray(new Tlv[0]);
+		return tlvArray;
+	}
+
+}
---
+++ leshan-client/src/main/java/leshan/client/lwm2m/resource/ClientResource.java
@@ -0,0 +1,29 @@
+package leshan.client.lwm2m.resource;
+
+import leshan.server.lwm2m.tlv.Tlv;
+import leshan.server.lwm2m.tlv.TlvType;
+import ch.ethz.inf.vs.californium.server.resources.ResourceBase;
+
+class ClientResource extends ResourceBase{
+
+	private final byte[] value;
+
+
+	public ClientResource(final int id, final byte[] value) {
+		super(Integer.toString(id));
+		this.value = value;
+	}
+
+	public int getId() {
+		return Integer.parseInt(getName());
+	}
+
+	public byte[] getValue() {
+		return value;
+	}
+
+	public Tlv asTlv() {
+		return new Tlv(TlvType.RESOURCE_VALUE, null, getValue(), getId());
+	}
+
+}
