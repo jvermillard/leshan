59f543d404da82925a989d9ce0a1b6c3cbc1e186
John Schloman <jschloman@zebra.com>
2014-10-16 17:31:19 -0500

    License header to all tests in leshan client.

--- leshan-client/src/test/java/leshan/client/lwm2m/resource/LwM2mObjectInstanceTest.java
+++ leshan-client/src/test/java/leshan/client/lwm2m/resource/LwM2mObjectInstanceTest.java
@@ -1,8 +1,37 @@
+/*
+ * Copyright (c) 2013, Sierra Wireless,
+ * Copyright (c) 2014, Zebra Technologies,
+ * 
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of {{ project }} nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package leshan.client.lwm2m.resource;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -30,7 +59,7 @@ public class LwM2mObjectInstanceTest {
 
 	@Test
 	public void testSingleResource() {
-		int resourceId = 12;
+		final int resourceId = 12;
 		initializeObjectWithSingleResource(resourceId, "hello");
 
 		assertCorrectRead(createInstance(definition, new byte[0]),
@@ -39,7 +68,7 @@ public class LwM2mObjectInstanceTest {
 
 	@Test
 	public void testMultipleResourceWithOneInstance() {
-		int resourceId = 65;
+		final int resourceId = 65;
 		initializeObjectWithMultipleResource(resourceId,
 				Collections.singletonMap(94, "ninety-four".getBytes()));
 
@@ -51,8 +80,8 @@ public class LwM2mObjectInstanceTest {
 
 	@Test
 	public void testMultipleResourceWithThreeInstances() {
-		int resourceId = 65;
-		Map<Integer, byte[]> values = new HashMap<>();
+		final int resourceId = 65;
+		final Map<Integer, byte[]> values = new HashMap<>();
 		values.put(1100, "eleven-hundred".getBytes());
 		values.put(10, "ten".getBytes());
 		values.put(3, "three".getBytes());
@@ -67,27 +96,27 @@ public class LwM2mObjectInstanceTest {
 				}, null, resourceId));
 	}
 
-	private void initializeObjectWithSingleResource(int resourceId, String value) {
+	private void initializeObjectWithSingleResource(final int resourceId, final String value) {
 		definition = new LwM2mClientObjectDefinition(100, MANDATORY, SINGLE,
 				new SingleResourceDefinition(resourceId, new SampleSingleResource(value), !REQUIRED));
 	}
 
-	private void initializeObjectWithMultipleResource(int resourceId, Map<Integer, byte[]> values) {
+	private void initializeObjectWithMultipleResource(final int resourceId, final Map<Integer, byte[]> values) {
 		definition = new LwM2mClientObjectDefinition(101, MANDATORY, SINGLE,
 				new SingleResourceDefinition(resourceId, new SampleMultipleResource(values), !REQUIRED));
 	}
 
-	private void assertCorrectRead(LwM2mClientObjectInstance instance, Tlv... tlvs) {
-		LwM2mExchange exchange = mock(LwM2mExchange.class);
+	private void assertCorrectRead(final LwM2mClientObjectInstance instance, final Tlv... tlvs) {
+		final LwM2mExchange exchange = mock(LwM2mExchange.class);
 		instance.read(exchange);
-		byte[] bytes = TlvEncoder.encode(tlvs).array();
+		final byte[] bytes = TlvEncoder.encode(tlvs).array();
 		verify(exchange).respond(ReadResponse.success(bytes));
 	}
 
-	private LwM2mClientObjectInstance createInstance(LwM2mClientObjectDefinition definition, byte[] payload) {
-		LwM2mClientObject obj = mock(LwM2mClientObject.class);
-		LwM2mClientObjectInstance instance = new LwM2mClientObjectInstance(0, obj, definition);
-		LwM2mCallbackExchange<LwM2mClientObjectInstance> createExchange = mock(LwM2mCallbackExchange.class);
+	private LwM2mClientObjectInstance createInstance(final LwM2mClientObjectDefinition definition, final byte[] payload) {
+		final LwM2mClientObject obj = mock(LwM2mClientObject.class);
+		final LwM2mClientObjectInstance instance = new LwM2mClientObjectInstance(0, obj, definition);
+		final LwM2mCallbackExchange<LwM2mClientObjectInstance> createExchange = mock(LwM2mCallbackExchange.class);
 		when(createExchange.getRequestPayload()).thenReturn(payload);
 		instance.createInstance(createExchange);
 		return instance;
@@ -95,14 +124,14 @@ public class LwM2mObjectInstanceTest {
 
 	private class SampleSingleResource extends StringLwM2mResource {
 
-		private String value;
+		private final String value;
 
-		public SampleSingleResource(String value) {
+		public SampleSingleResource(final String value) {
 			this.value = value;
 		}
 
 		@Override
-		public void handleRead(StringLwM2mExchange exchange) {
+		public void handleRead(final StringLwM2mExchange exchange) {
 			exchange.respondContent(value);
 		}
 
@@ -110,14 +139,14 @@ public class LwM2mObjectInstanceTest {
 
 	private class SampleMultipleResource extends MultipleLwM2mResource {
 
-		private Map<Integer, byte[]> values;
+		private final Map<Integer, byte[]> values;
 
-		public SampleMultipleResource(Map<Integer, byte[]> values) {
+		public SampleMultipleResource(final Map<Integer, byte[]> values) {
 			this.values = values;
 		}
 
 		@Override
-		public void handleRead(MultipleLwM2mExchange exchange) {
+		public void handleRead(final MultipleLwM2mExchange exchange) {
 			exchange.respondContent(values);
 		}
 
