a177d58737289c60c8486b86188798c62845a2f2
Simon Bernard <sbernard@sierrawireless.com>
2014-11-20 19:04:30 +0100

    #46: add a way to add custom objects definition

--- leshan-core/src/main/java/leshan/core/objectspec/Resources.java
+++ leshan-core/src/main/java/leshan/core/objectspec/Resources.java
@@ -29,10 +29,13 @@
  */
 package leshan.core.objectspec;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -64,7 +67,7 @@ public class Resources {
         synchronized (OBJECTS) {
             if (OBJECTS.isEmpty()) {
 
-                // load OMA objects definitions from XML resource files
+                // load OMA objects definitions from json files
                 InputStream input = Resources.class.getResourceAsStream("/objectspec.json");
                 if (input == null) {
                     return;
@@ -82,6 +85,55 @@ public class Resources {
                     }
                 } catch (IOException e) {
                     LOG.error("Unable to load object specification", e);
+                }
+
+                // load custom resources
+                // get folder path
+                String modelsFolderEnvVar = System.getenv("MODELS_FOLDER");
+                String modelsFolderPath = modelsFolderEnvVar != null ? modelsFolderEnvVar : "./models";
+
+                // check if the folder is usable
+                File modelsFolder = new File(modelsFolderPath);
+                if (!modelsFolder.isDirectory() || !modelsFolder.canRead()) {
+                    // log only if env var is configured
+                    if (modelsFolderEnvVar != null)
+                        LOG.error(MessageFormat.format(
+                                "Models folder {0} is not a directory or you have not the right to list its content.",
+                                modelsFolderPath));
+                    return;
+                }
+
+                // get all files
+                for (File file : modelsFolder.listFiles()) {
+                    if (!file.canRead())
+                        continue;
+
+                    if (file.getName().endsWith(".xml")) {
+                        // load DDF file
+                        DDFFileParser ddfFileParser = new DDFFileParser();
+                        ObjectSpec objectSpec = ddfFileParser.parse(file);
+                        if (objectSpec != null) {
+                            if (OBJECTS.containsKey(objectSpec.id))
+                                LOG.warn(MessageFormat.format("There are multiple definitions for the object {0}",
+                                        objectSpec.id));
+                            OBJECTS.put(objectSpec.id, objectSpec);
+                        }
+                    } else if (file.getName().endsWith(".json")) {
+                        // load object spec json file
+                        try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
+                            ObjectSpec[] objectSpecs = gson.fromJson(reader, ObjectSpec[].class);
+                            for (ObjectSpec objectSpec : objectSpecs) {
+                                if (OBJECTS.containsKey(objectSpec.id))
+                                    LOG.warn(MessageFormat.format("There are multiple definitions for the object {0}",
+                                            objectSpec.id));
+                                OBJECTS.put(objectSpec.id, objectSpec);
+                            }
+                        } catch (IOException e) {
+                            LOG.warn(
+                                    MessageFormat.format("Unable to load object specification for {0}",
+                                            file.getAbsolutePath()), e);
+                        }
+                    }
                 }
             }
         }
