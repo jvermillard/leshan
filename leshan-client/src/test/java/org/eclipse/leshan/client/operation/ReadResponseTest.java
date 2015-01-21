4584920c09dcc7dc9038858917632fa8ae7cbffc
Simon Bernard <sbernard@sierrawireless.com>
2015-01-21 16:26:24 +0100

    Initial Contrib : change copyright/licence headers

--- leshan-client/src/test/java/org/eclipse/leshan/client/operation/ReadResponseTest.java
+++ leshan-client/src/test/java/org/eclipse/leshan/client/operation/ReadResponseTest.java
@@ -1,34 +1,13 @@
-/*
- * Copyright (c) 2013, Sierra Wireless,
- * Copyright (c) 2014, Zebra Technologies,
- * 
+/*******************************************************************************
+ * Copyright (c) 2013-2015 Sierra Wireless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright notice,
- *       this list of conditions and the following disclaimer in the documentation
- *       and/or other materials provided with the distribution.
- *     * Neither the name of {{ project }} nor the names of its contributors
- *       may be used to endorse or promote products derived from this software
- *       without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+ * Contributors:
+ *     Zebra Technologies - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.leshan.client.operation;
 
 import static org.junit.Assert.assertArrayEquals;
--- leshan-standalone/src/main/resources/webapp/js/client-controllers.js
+++ leshan-standalone/src/main/resources/webapp/js/client-controllers.js
@@ -1,8 +1,13 @@
-/*!
- * Copyright (c) 2013-2014, Sierra Wireless
- * Released under the BSD license
- * https://raw.githubusercontent.com/jvermillard/leshan/master/LICENSE
- */
+/*******************************************************************************
+ * Copyright (c) 2013-2015 Sierra Wireless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
 
 var lwClientControllers = angular.module('clientControllers', []);
 
